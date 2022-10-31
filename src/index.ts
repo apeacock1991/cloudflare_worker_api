d3ef3bb55548e4d97f913de1fb8d24f8a2790884
Ashley Peacock <ashley.peacock@simplybusiness.co.uk>
2022-10-31 14:55:41 +0000

    Add persistence

--- src/index.ts
+++ src/index.ts
@@ -8,45 +8,71 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-import { Router } from 'itty-router'
+// Import request using an alias to avoid collisions with Cloudflare's implementation of Request in the fetch method
+import { Router, Request as IttyRequest } from 'itty-router'
 
 const router = Router()
 
-// Define a simple data set for our API
-const animals = [
-	{
-		name: 'cow',
-		id: 1,
-		type: 'mammal'
-	},
-	{
-		name: 'falcon',
-		id: 2,
-		type: 'bird'
-	},
-	{
-		name: 'salmon',
-		id: 3,
-		type: 'fish'
-	}
-]
+const ANIMALS_KV_KEY_ID = 'all-animals'
 
 // Register the route to return all the animals
-router.get('/animals', (req) => {
+router.get('/animals', async (req: IttyRequest, env: Env) => {
+	let animals = await getAllAnimals(env)
+
 	return new Response(JSON.stringify(animals))
 })
 
 // Register the route to return an animal by ID
-router.get('/animals/:id', (req) => {
-	const { params, query } = req
+router.get('/animals/:id', async (req: IttyRequest, env: Env) => {
+	let animals = await getAllAnimals(env)
 
-	let animal = animals.find(a => a.id == Number(params?.id))
+	let animal = animals.find(a => a['id'] == Number(req.params?.id))
+
+	return new Response(JSON.stringify(animal))
+})
+
+// Register the route to create an animal
+router.post('/animals', async (req: IttyRequest, env: Env) => {
+	let content = await req.json?.()
+
+	if (content == undefined) {
+		return new Response('Please provide a body.')
+	}
+
+	//Do not use this in production, use a UUID, using Date.now() for convenience
+	content['id'] = Date.now()
+
+	let animal = await addAnimal(content, env)
 
 	return new Response(JSON.stringify(animal))
 })
 
 // Return a 404 for everything else
 router.all('*', () => new Response('Not Found.', { status: 404 }))
+
+export async function getAllAnimals(env: Env): Promise<any[]> {
+	let animals = await env.ANIMALS.get(ANIMALS_KV_KEY_ID)
+
+	if (animals === null) {
+		return []
+	}
+
+	return JSON.parse(animals);
+}
+
+export async function updateAnimals(animals: any[], env: Env) {
+	await env.ANIMALS.put(ANIMALS_KV_KEY_ID, JSON.stringify(animals))
+}
+
+export async function addAnimal(animal: Object, env: Env) {
+	let animals = await getAllAnimals(env)
+
+	animals.push(animal);
+
+	await updateAnimals(animals, env)
+
+	return animal;
+}
 
 export interface Env {
 	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
@@ -57,6 +83,10 @@ export interface Env {
 	//
 	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
 	// MY_BUCKET: R2Bucket;
+
+	//This will be auto-populated with the KV Namespace that is bound in the wrangler.toml
+	//and exposes all the methods you'll need (get, put, list etc.)
+	ANIMALS: KVNamespace;
 }
 
 export default {
@@ -65,6 +95,6 @@ export default {
 		env: Env,
 		ctx: ExecutionContext
 	): Promise<Response> {
-		return await router.handle(request)
-	},
+		return await router.handle(request, env)
+	}
 };
